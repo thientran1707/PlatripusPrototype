5bdf8abb1c5526357be7a5a35f223ba9b7e8b2f3
Thien Tran <thientran1707@gmail.com>
2016-06-07 12:03:11 -0700

    add template

---
+++ Platripus/Platripus-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Platripus-Bridging-Header.h
+//  Platripus
+//
+//  Created by Thien Tran on 6/7/16.
+//  Copyright © 2016 NVC_2016. All rights reserved.
+//
+
+#import "SWRevealViewController.h"
