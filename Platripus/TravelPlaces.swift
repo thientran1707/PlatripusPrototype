189c5269ef5c828bd474d0984853a04bca2d5ead
sswander <leonardosyahputra@yahoo.com>
2016-06-11 14:25:51 -0700

    added more map codes
    Signed-off-by: sswander <leonardosyahputra@yahoo.com>

---
+++ Platripus/TravelPlaces.swift
@@ -0,0 +1,30 @@
+//
+//  TravelPlaces.swift
+//  Platripus
+//
+//  Created by Leonardo Sjahputra on 6/11/16.
+//  Copyright © 2016 NVC_2016. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class Artwork: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+}
