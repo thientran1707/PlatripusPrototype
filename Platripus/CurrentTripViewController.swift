2e1a06f6fb02082176e7ecc7123873298b320ae1
sswander <leonardosyahputra@yahoo.com>
2016-06-11 14:26:01 -0700

    codes to view annotations
    Signed-off-by: sswander <leonardosyahputra@yahoo.com>

--- Platripus/CurrentTripViewController.swift
+++ Platripus/CurrentTripViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class CurrentTripViewController: UIViewController {
     @IBOutlet weak var menuButton:UIBarButtonItem!
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +22,20 @@ class CurrentTripViewController: UIViewController {
             menuButton.action = "revealToggle:"
             view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        // set initial location in Honolulu
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        centerMapOnLocation(initialLocation)
+        
+        mapView.delegate = self
+        
+        // show artwork on map
+        let artwork = Artwork(title: "King David Kalakaua",
+            locationName: "Waikiki Gateway Park",
+            discipline: "Sculpture",
+            coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+        
+        mapView.addAnnotation(artwork)
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,6 +43,12 @@ class CurrentTripViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 
     /*
     // MARK: - Navigation
