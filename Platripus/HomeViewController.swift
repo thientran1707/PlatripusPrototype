6504b3b50b433aa472e9c2cdd0a2f7ddb30c2ab6
Thien Tran <thientran1707@gmail.com>
2016-06-11 21:00:51 -0700

    fix scrollview

--- Platripus/HomeViewController.swift
+++ Platripus/HomeViewController.swift
@@ -12,12 +12,13 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var menuButton: UIBarButtonItem!
     
-    @IBOutlet weak var helloText: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.automaticallyAdjustsScrollViewInsets = false
         
-        helloText.userInteractionEnabled = false
+        //helloText.userInteractionEnabled = false
+        //selectionView.layer.borderWidth = 1
+        //selectionView.layer.borderColor = UIColor.redColor().CGColor
         
         if revealViewController() != nil {
             menuButton.target = revealViewController()
